ced81e7149ecf3b1f0585cee59bed905a8bd6c72
zhaoxianh <zhaoxianh@chanjet.com>
2020-11-20 18:00:35 +0800

    首次提交代码

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// eslint-disable-next-line 
+import {PageDecorator} from './PageDecorator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+@PageDecorator
+class App extends Component {
+   render(){
+     return (
+        <div style={{width:'400px', height:'300px'}}>
+           <p>我是App页面</p>
+           <p>{this.props.value}</p>
+           <p>{`我来自${this.props.address}`}</p>
+        </div>     
+     )
+   }
 }
 
 export default App;
---
+++ src/PageDecorator.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export const PageDecorator = (WrappedComponent) =>{
+
+    return class PageNormal extends Component{
+        state = {
+            address: '河北省承德市',
+            age: 25,
+        }
+        render(){
+
+            return (
+
+                <div style={{backgroundColor:'skyblue'}}>
+
+                    <WrappedComponent {...this.props} value={"装饰器传过来的值"} address={this.state.address}></WrappedComponent>
+
+                </div>
+
+            )
+
+        }
+
+    }
+
+}
